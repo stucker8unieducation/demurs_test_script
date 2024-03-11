b65082508766a5736070befa4f6e3a1dd491d078
Staccate <69740785+stucker8unieducation@users.noreply.github.com>
2024-03-11 17:57:27 +0900

    Add files via upload

---
+++ demcus_test.py
@@ -0,0 +1,5 @@
+import demucs.separate
+
+file_path = "/Users/stac2299/Library/CloudStorage/OneDrive-公立諏訪東京理科大学/新しいフォルダー/audio_files/goodbye.wav"
+
+demucs.separate.main(["-n", "htdemucs_ft", file_path,])
